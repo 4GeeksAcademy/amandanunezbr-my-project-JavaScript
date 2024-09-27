a65d548442cbcc5476d8edf62034e4c838ee8c75
amandanunezbr <gerencialmonsters@gmail.com>
2024-09-27 19:05:28 +0000

    No funciona

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function generador() {
+    const initialPhrase = who[Math.floor(Math.random() * who.length)];
+    const actionsPhrase = action[Math.floor(Math.random() * action.length)];
+    const whatsPhrase = what[Math.floor(Math.random() * what.length)];
+    const whensPhrase = when[Math.floor(Math.random() * when.length)];
+
+    const finalResult = `${initialPhrase} ${actionsPhrase} ${whatsPhrase} ${whensPhrase}`;
+
+    document.getElementById("resultado").innerText = finalResult;
+  }
+
+  document.getElementById("generador").addEventListener("click", () => {
+    console.log("excusa!");
+    generador();
+  });
 };
